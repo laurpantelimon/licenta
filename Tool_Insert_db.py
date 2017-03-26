003f2f4df712d59bd7dd9c146becf9b1afaaaab8
robertvelcu <robert.velcu@yahoo.com>
2017-03-26 17:58:12 +0300

    new stuff
    Added some menus, buttons and checkboxes

--- Tool_Insert_db.py
+++ Tool_Insert_db.py
@@ -2,25 +2,54 @@ import ttk
 from Tkinter import *
 import pypyodbc
 import ScrolledText
+import tkMessageBox
+from PIL import ImageTk, Image
 
+def db_schema():
+    window = Toplevel(root)
+    img = ImageTk.PhotoImage(Image.open("D://robertv//Fac//Licenta//db_schema.png"))
+    panel = Label(window, image=img)
+    panel.pack(side="bottom", fill="both", expand="yes")
+    root.wait_window(window)
 
-# #Database conection
-# connection2 = pypyodbc.connect('Driver={SQL Server};'
-#                                'Server=.\SQLEXPRESS;'
-#                                'Database=NaturalSQL;'
-#                                'Trusted_Connection=yes;')
-# cursor2 = connection2.cursor()
+def makemenu(win):
+    top = Menu(win)  # win=top-level window
+    win.config(menu=top)  # set its menu option
+
+    edit = Menu(top, tearoff=0)
+    top.add_cascade(label='Tool', menu=edit, underline=0)
+
+    submenu = Menu(edit, tearoff=0)
+    submenu.add_command(label='Database Schema', command=db_schema, underline=0)
+    submenu.add_command(label='Exit', command=exit, underline=0)
+    edit.add_cascade(label='Actions', menu=submenu, underline=0)
+
+def skip_command():
+    result = tkMessageBox.askquestion("Skip query", "Are You Sure?", icon='warning')
+    if result == 'yes':
+        print "SKIP"
+    else:
+        print "Don't skip"
+
+#Database conection
+connection2 = pypyodbc.connect('Driver={SQL Server};'
+                                'Server=.\SQLEXPRESS;'
+                                'Database=NaturalSQL;'
+                                'Trusted_Connection=yes;')
+cursor2 = connection2.cursor()
 
 #GUI Generator
 root = Tk(className ="Insert queries")
 root.title("CSD Update Error DB")
 root.geometry('1200x800')
 root.configure(background='dark grey')
+makemenu(root)
 
 nlFrame = ttk.Frame(root)
 nlFrame.pack(side = 'left',fill = Y)
 
 scrollList = []
+
 
 for i in range(10):
 
@@ -61,11 +90,17 @@ w3.pack(side = 'top')
 l2 = Label(root, text="Name",font = ("Helvetica",12))
 l2.pack(side = 'top')
 
+var1 = IntVar()
+cb = Checkbutton(root, text="Complex query", variable=var1)
+cb.pack(side = 'top',fill = Y)
+
+
 #Insert values in database
 def act():
 
     for i in range(10):
 
+        difficult_query = var1.get()
         natural_query = scrollList[i].get('1.0', END)
         sql_query = w2.get('1.0', END)
         query_name = w3.get('1.0', END)
@@ -83,9 +118,10 @@ buttonsFrame.pack(side = 'bottom', fill = Y)
 
 insert = Button(buttonsFrame, text="INSERT",font = ("Helvetica",15),command=act, height = 5, width = 10, bg = 'dark grey')
 insert.pack(side = 'left')
-skip = Button(buttonsFrame, text="SKIP", font = ("Helvetica",15),command=act, height = 5, width = 10, bg = 'dark grey')
+skip = Button(buttonsFrame, text="SKIP", font = ("Helvetica",15),command=skip_command, height = 5, width = 10, bg = 'dark grey')
 skip.pack(side = 'left')
 
 root.mainloop()
-# cursor2.close()
-# connection2.close()
+cursor2.commit()
+cursor2.close()
+connection2.close()
